f3bcf9e993c65d204f538249e64efced8dcf037b
chien.pham <chien.pham@spiraledge.com>
2022-07-25 20:00:30 +0700

    create emptyState for create qr code

--- web/frontend/pages/index.jsx
+++ web/frontend/pages/index.jsx
@@ -1,86 +1,50 @@
 import {
-  Card,
-  Page,
-  Layout,
-  TextContainer,
-  Image,
-  Stack,
-  Link,
-  Heading,
+    Card,
+    EmptyState,
+    Layout,
+    Page,
+    SkeletonBodyText,
 } from "@shopify/polaris";
-import { TitleBar } from "@shopify/app-bridge-react";
+import { TitleBar, useNavigate, Loading } from "@shopify/app-bridge-react";
 
 import { trophyImage } from "../assets";
 
 import { ProductsCard } from "../components";
 
 export default function HomePage() {
-  return (
-    <Page narrowWidth>
-      <TitleBar title="Hi there" primaryAction={null} />
-      <Layout>
-        <Layout.Section>
-          <Card sectioned>
-            <Stack
-              wrap={false}
-              spacing="extraTight"
-              distribution="trailing"
-              alignment="center"
-            >
-              <Stack.Item fill>
-                <TextContainer spacing="loose">
-                  <Heading>Nice work on building a Shopify app 🎉</Heading>
-                  <p>
-                    Your app is ready to explore! It contains everything you
-                    need to get started including the{" "}
-                    <Link url="https://polaris.shopify.com/" external>
-                      Polaris design system
-                    </Link>
-                    ,{" "}
-                    <Link url="https://shopify.dev/api/admin-graphql" external>
-                      Shopify Admin API
-                    </Link>
-                    , and{" "}
-                    <Link
-                      url="https://shopify.dev/apps/tools/app-bridge"
-                      external
-                    >
-                      App Bridge
-                    </Link>{" "}
-                    UI library and components.
-                  </p>
-                  <p>
-                    Ready to go? Start populating your app with some sample
-                    products to view and test in your store.{" "}
-                  </p>
-                  <p>
-                    Learn more about building out your app in{" "}
-                    <Link
-                      url="https://shopify.dev/apps/getting-started/add-functionality"
-                      external
-                    >
-                      this Shopify tutorial
-                    </Link>{" "}
-                    📚{" "}
-                  </p>
-                </TextContainer>
-              </Stack.Item>
-              <Stack.Item>
-                <div style={{ padding: "0 20px" }}>
-                  <Image
-                    source={trophyImage}
-                    alt="Nice work on building a Shopify app"
-                    width={120}
-                  />
-                </div>
-              </Stack.Item>
-            </Stack>
-          </Card>
-        </Layout.Section>
-        <Layout.Section>
-          <ProductsCard />
-        </Layout.Section>
-      </Layout>
-    </Page>
-  );
+
+    const navigate = useNavigate();
+
+    const isLoading = false;
+    const QRcodes = [];
+
+    const loadingMarkup = isLoading ? (
+        <Card sectioned>
+            <Loading />
+            <SkeletonBodyText />
+        </Card>
+    ) : null;
+
+    const emptyStateMarkup = !isLoading && !QRcodes.length ? (
+        <EmptyState
+            heading="Create unique QR codes for your product"
+            footerContent="Allow customers to scan codes and buy products using their phones"
+            image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
+            action={{ content: "Create QR code" }}
+        ></EmptyState>
+    ) : null;
+    return (
+        <Page>
+            <TitleBar
+                title="QR codes"
+                primaryAction={{ content: "Create QR code" }}
+            ></TitleBar>
+            <Layout>
+                <Layout.Section>
+                    {loadingMarkup}
+                    {emptyStateMarkup}
+                </Layout.Section>
+            </Layout>
+        </Page>
+    );
 }
