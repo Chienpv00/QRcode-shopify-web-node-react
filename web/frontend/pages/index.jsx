02653f2076fe0f9a5ba2a4305bdeed30f06c94d1
chien pham <chien.pham@spiraledge.com>
2022-08-01 23:28:19 -0700

    step 4

--- web/frontend/pages/index.jsx
+++ web/frontend/pages/index.jsx
@@ -4,19 +4,14 @@ import {
     Layout,
     Page,
     SkeletonBodyText,
-} from "@shopify/polaris";
-import { TitleBar, useNavigate, Loading } from "@shopify/app-bridge-react";
-
-import { trophyImage } from "../assets";
-
-import { ProductsCard } from "../components";
+} from '@shopify/polaris';
+import { TitleBar, useNavigate, Loading } from '@shopify/app-bridge-react';
 
 export default function HomePage() {
-
     const navigate = useNavigate();
-
     const isLoading = false;
-    const QRcodes = [];
+    const isRefetching = false;
+    const QRCodes = [];
 
     const loadingMarkup = isLoading ? (
         <Card sectioned>
@@ -24,27 +19,39 @@ export default function HomePage() {
             <SkeletonBodyText />
         </Card>
     ) : null;
-
-    const emptyStateMarkup = !isLoading && !QRcodes.length ? (
-        <EmptyState
-            heading="Create unique QR codes for your product"
-            footerContent="Allow customers to scan codes and buy products using their phones"
-            image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
-            action={{ content: "Create QR code" }}
-        ></EmptyState>
-    ) : null;
+    const emptyStateMarkup =
+        !isLoading && !QRCodes?.length ? (
+            <Card sectioned>
+                <EmptyState
+                    image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
+                    action={{
+                        content: 'Create QR code',
+                        onAction: () => navigate('/qrcodes/new'),
+                    }}
+                >
+                    <p>
+                        Allow customers to scan codes and buy products using
+                        their phones.
+                    </p>
+                </EmptyState>
+            </Card>
+        ) : null;
     return (
-        <Page>
-            <TitleBar
-                title="QR codes"
-                primaryAction={{ content: "Create QR code" }}
-            ></TitleBar>
-            <Layout>
-                <Layout.Section>
-                    {loadingMarkup}
-                    {emptyStateMarkup}
-                </Layout.Section>
-            </Layout>
-        </Page>
-    );
+		<Page>
+			<TitleBar
+				title='QR codes'
+				primaryAction={{
+					content: 'Create QR code',
+					onAction: () => navigate('/qrcodes/new')
+				}}
+			>
+			</TitleBar>
+			<Layout>
+				<Layout.Section>
+					{loadingMarkup}
+					{emptyStateMarkup}
+				</Layout.Section>
+			</Layout>
+		</Page>
+	);
 }
